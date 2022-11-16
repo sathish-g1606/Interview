be97ef2acdeb445245cda829e60b53305c1b02ab
SathishKumar Gnanasekaran <71622340+sathish-g1606@users.noreply.github.com>
2022-11-16 09:25:07 +0530

    Add files via upload

---
+++ Atleast5.js
@@ -0,0 +1,7 @@
+const logAtLeast5 = (n) => {
+    for (let i = 1; i <= Math.max(5, n); i++) {
+        console.log(i)
+    }
+}
+
+logAtLeast5(1); // O(n)
---
+++ Atmost5.js
@@ -0,0 +1,7 @@
+const logAtMost5 = (n) => {
+    for (let i = 1; i <= Math.min(5, n); i++) {
+        console.log(i)
+    }
+}
+
+logAtMost5(10); // O(1)
